41d8644ce8227de49b96a8f000832c81e8edbb12
Felipe <felipe.floresv@alumnos.usm.cl>
2018-10-24 15:05:58 -0300

    add multiple frequency curve endpoint

--- web/controllers/visualization_controller.ex
+++ web/controllers/visualization_controller.ex
@@ -86,6 +86,74 @@ defmodule Bff.VisualizationController do
     end    
   end
 
+  def get_multiple_frequency_curve(conn, %{"topics_ids" => topics_ids, "date" => date}) do
+    header = [
+              {"Content-Type", "application/json"}
+             ]
+
+    topics_ids = String.split(topics_ids, ",")
+    case HTTPoison.get("http://business-rules:8001/dateConversion/#{date}/", header, []) do
+      {:ok, %HTTPoison.Response{body: dates_body}} ->
+
+        dates_hash_response = Poison.decode!(dates_body)
+
+
+        hash_with_integer = Enum.map(dates_hash_response, fn v -> 
+          {v["week"], v["sunday_date"]} 
+        end)
+        |> Map.new
+
+
+        sunday_array = Enum.map(dates_hash_response, fn v -> 
+          v["sunday_date"]
+        end)
+
+
+        [first_date | _] = dates_hash_response
+
+        responses_array = %{ topics: Enum.map(topics_ids, fn topic_id ->
+          filters = [%{type: "nested", path: "topics", queries: [ %{ type: "match", field: "id", value: topic_id } ] }]
+
+          filters = Poison.encode! filters
+
+          grouping = [%{type: "range", key: "int_published",  opts: %{ step: 1, min: first_date["week"], max: (first_date["week"] + 23) } }]
+          grouping = Poison.encode! grouping
+
+          query = "http://categorized_data:4000/api/documents/?page_size=30&filters=" <> filters <> "&grouping=" <> grouping
+
+          case HTTPoison.get(query, header, []) do
+            {:ok, %HTTPoison.Response{body: body}} ->
+              hash_response = Poison.decode!(body)
+              value = first_date["week"] - 1
+              hash_with_buckets_and_dates = Enum.zip(sunday_array, hash_response["buckets"]) |> Enum.into(%{})
+              response = %{topic_id: topic_id, weeks: Enum.map(hash_with_buckets_and_dates ,fn {k, v} -> 
+
+                %{week: k, count: v["data"]["document_count"]} 
+              end)}
+
+            {:error, _response} ->
+
+              %{error: "No fue posible consultar información para el topico #{topic_id}", topic_id: topic_id}
+          end
+
+        end)
+      }
+
+
+
+      IO.inspect responses_array
+      conn
+      |> put_status(200)
+      |> render(Bff.WormholeView, "tunnel.json", %{data: responses_array})
+
+
+      {:error, _response} ->
+        conn
+        |> put_status(401)
+        |> render(Bff.ErrorView, "500.json")
+
+    end    
+  end
 
   # def get_hot_topics(conn, %{"topics_ids" => topics_ids}) do
   #   header = [
--- web/router.ex
+++ web/router.ex
@@ -39,6 +39,7 @@ defmodule Bff.Router do
     get "/visualizations/graph", VisualizationController, :get_graph
     get "/visualizations/frequency_curve", VisualizationController, :get_frequency_curve
     get "/visualizations/hot_topics", VisualizationController, :get_hot_topics
+    get "/visualizations/multiple_frequency_curve", VisualizationController, :get_multiple_frequency_curve
     get "/idms/dashboard", IdmController, :get_dashboard
     put "/idms/dashboard", IdmController, :update_dashboard
     delete "/idms/dashboard", IdmController, :delete_dashboard
