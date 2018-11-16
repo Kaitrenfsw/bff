f2e14cbdfeedf62eabb36844d2adc58073c466d8
Felipe <felipe.floresv@alumnos.usm.cl>
2018-11-15 23:44:39 -0300

    fixed problem with route to login log

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -15,7 +15,7 @@ defmodule Bff.UserController do
           data = hash_response["data"]
 
           body_tracer = Poison.encode!(%{user_id: data["id"]})
-          case HTTPoison.post("http://tracer:4000/api/login_log/", body_tracer, header, []) do
+          case HTTPoison.post("http://tracer:4000/api/login_logs/", body_tracer, header, []) do
 
             {:ok, %HTTPoison.Response{body: body}} ->
               conn
