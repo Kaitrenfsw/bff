1063347ddfd6758f5a4c9559662d5e0963022fca
Felipe <felipe.floresv@alumnos.usm.cl>
2018-11-20 22:05:12 -0300

    Add endpoint to get bar chart

--- web/router.ex
+++ web/router.ex
@@ -55,7 +55,7 @@ defmodule Bff.Router do
     delete "/remove_content_user", VoteController, :remove_content_user
     get "/saved_news", VoteController, :saved_news
     get "/get_sources", VoteController, :get_sources
-    get "idms_sources", VoteController, :get_topic_stats
+    get "/idms_sources", VoteController, :get_topic_stats
 
   end
 end
